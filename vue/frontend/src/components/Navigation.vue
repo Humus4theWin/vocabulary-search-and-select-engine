7e7bef20783cbbe26743629db6a2f412e4671342
cim82 <m.wrusch@freenet.de>
2021-06-01 15:22:50 +0200

    removed home button from bottom navigation

--- vue/frontend/src/components/Navigation.vue
+++ vue/frontend/src/components/Navigation.vue
@@ -1,18 +1,6 @@
 <template>
   <v-bottom-navigation class="primary" absolute min-height="80" max-height="85">
-    <v-btn
-      class="primary"
-      :to="{ name: 'Home' }"
-      depressed
-      min-height="80"
-      max-height="85"
-      width="80"
-    >
-      <span>Home</span>
-
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
-
+    
     <v-btn
       class="primary"
       :to="{ name: 'Search' }"
