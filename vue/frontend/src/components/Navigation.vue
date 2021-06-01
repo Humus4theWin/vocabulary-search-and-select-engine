ab301f54851c01f89e7a657e1c4ba13f0af80fe4
cim82 <m.wrusch@freenet.de>
2021-06-01 15:18:40 +0200

    added link to capabilitie from proceed

--- vue/frontend/src/components/Navigation.vue
+++ vue/frontend/src/components/Navigation.vue
@@ -22,9 +22,17 @@
       width="80"
     >
       <span>Search</span>
-
-      <v-icon>mdi-library</v-icon>
+<v-icon>mdi-library</v-icon>
+      
     </v-btn>
+    <a href=https://docs.proceed-labs.org/concepts/capability-description/>
+    
+    <v-btn class="primary" depressed min-height="80" max-height="85" width="80">
+      <span>Proceed</span>
+      <v-icon>mdi-library</v-icon>
+
+      
+    </v-btn></a>
   </v-bottom-navigation>
 </template>
 
