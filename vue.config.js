b44a868bfeed811a2a8dc4c506eea70bd8e2d27b
Michael Wrusch <m.wrusch@freenet.de>
2021-06-13 23:54:23 +0200

    CI change path in vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/vocabulary-search-and-select-engine/"
+      ? "/vocabulary-search-and-select-engine/vue/frontend"
       : "/",
 };
