9f796850b68bc09bdc18cc7473e7174ef0391c32
dimitristaufer <staufer@campus.tu-berlin.de>
2021-06-19 10:39:53 +0200

    CICD file edit

---
+++ src/defaultVocPipeline/downloadParseCommit.js
@@ -0,0 +1,13 @@
+/**
+ * @file Pipeline file to downloads and parse a hard-coded collection of vocabularies and commit them to our repository if changes occur.
+ * @author Dimitri Jan Staufer
+ */
+
+console.log("Ok");
+
+let defaultVocabularies = [
+  { name: "", downloadURL: "" },
+  { name: "", downloadURL: "" },
+];
+
+console.log(defaultVocabularies);
