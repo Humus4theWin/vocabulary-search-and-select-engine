7bd062261de74a67e1926e3896800c1bf0a94789
Linus Pfoch <linus@pfoch.de>
2021-06-28 11:37:49 +0200

    Filter Table + edit/add

--- src/components/FilterTable.vue
+++ src/components/FilterTable.vue
@@ -26,22 +26,25 @@
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field
+                    <v-select
+                      :items="[true, false]"
                       v-model="editedItem.isUsed"
                       label="in use"
-                    ></v-text-field>
+                    ></v-select>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field
+                    <v-select
+                      :items="searchTypes"
                       v-model="editedItem.searchType"
                       label="Search type"
-                    ></v-text-field>
+                    ></v-select>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field
+                    <v-select
+                      :items="predicates"
                       v-model="editedItem.predicate"
                       label="Predicate"
-                    ></v-text-field>
+                    ></v-select>
                   </v-col>
                 </v-row>
               </v-container>
@@ -49,24 +52,24 @@
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+              <v-btn color="blue darken-1" text @click="close"> Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save"> Save</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5"
-              >Are you sure you want to delete this item?</v-card-title
-            >
+              >Are you sure you want to delete this item?
+            </v-card-title>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel</v-btn
-              >
+                >Cancel
+              </v-btn>
               <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >OK</v-btn
-              >
+                >OK
+              </v-btn>
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
@@ -74,11 +77,11 @@
       </v-toolbar>
     </template>
     <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil</v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete</v-icon>
     </template>
     <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset </v-btn>
+      <v-btn color="primary" @click="initialize"> Reset</v-btn>
     </template>
   </v-data-table>
 </template>
@@ -137,6 +140,9 @@ export default {
   methods: {
     initialize() {
       this.filterCriteria = this.$store.getters.getFilterCriteria;
+      this.predicates = [
+        ...new Set(Object.keys(...this.$store.getters.getVocabTerms)),
+      ];
     },
 
     editItem(item) {
